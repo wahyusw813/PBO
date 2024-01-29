9c623ad9689f8ed35dca700f0e3898c3540fb23a
Wahyu Satrio Widodo <74386472+wahyusw813@users.noreply.github.com>
2024-01-29 07:55:50 +0700

    Update testCylinder.java

--- Pertemuan3/CompositionCylinder/testCylinder.java
+++ Pertemuan3/CompositionCylinder/testCylinder.java
@@ -2,7 +2,6 @@ public class testComposeCircle {
     public static void main(String[] args) {
         Cylinder cylinder = new Cylinder(5.0, "Red", 10.0);
 
-        System.out.println("Area of the Circle: " + cylinder.calculateVolume());
         System.out.println("Volume of the Cylinder: " + cylinder.calculateVolume());
         System.out.println("Color of the Circle: " + cylinder.getColor());
         System.out.println("Height of the Cylinder: " + cylinder.getHeight());
