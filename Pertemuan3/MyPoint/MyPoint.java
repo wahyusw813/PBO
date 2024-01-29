3dcf62e0b7d729d630a3181812ed632525c954d3
Wahyu Satrio Widodo <74386472+wahyusw813@users.noreply.github.com>
2024-01-29 07:12:43 +0700

    Create MyPoint.java

---
+++ Pertemuan3/MyPoint/MyPoint.java
@@ -0,0 +1,61 @@
+public class MyPoint {
+    private int x;
+    private int y;
+
+    public MyPoint() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    public int[] getXY() {
+        int[] xy = {x, y};
+        return xy;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+
+    public double distance(int x, int y) {
+        int xDiff = this.x - x;
+        int yDiff = this.y - y;
+        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    }
+
+    public double distance(MyPoint another) {
+        int xDiff = this.x - another.getX();
+        int yDiff = this.y - another.getY();
+        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    }
+
+    public double distance() {
+        return Math.sqrt(x * x + y * y);
+    }
+}
