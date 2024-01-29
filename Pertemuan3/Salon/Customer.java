bbaefec37448c1deb2c448c36fb5c6fb213ffb9b
Wahyu Satrio Widodo <74386472+wahyusw813@users.noreply.github.com>
2024-01-29 07:59:21 +0700

    Create Customer.java

---
+++ Pertemuan3/Salon/Customer.java
@@ -0,0 +1,36 @@
+class Customer {
+    private String name;
+    private boolean member = false;
+    private String memberType;
+
+    public Customer(String name, boolean member, String memberType) {
+        this.name = name;
+        this.member = member;
+        this.memberType = memberType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isMember() {
+        return member;
+    }
+
+    public void setMember(boolean member) {
+        this.member = member;
+    }
+
+    public String getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(String memberType) {
+        this.memberType = memberType;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" + "name='" + name + '\'' + ", member=" + member + ", memberType='" + memberType + '\'' + '}';
+    }
+}
