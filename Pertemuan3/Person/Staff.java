f90497cffb08dd82860233816d671b188cb83cd8
Wahyu Satrio Widodo <74386472+wahyusw813@users.noreply.github.com>
2024-01-29 07:39:27 +0700

    Create Staff.java

---
+++ Pertemuan3/Person/Staff.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Person;
+
+/**
+ *
+ * @author Wahyu
+ */
+public class Staff extends Person {
+    private String school;
+    private double pay;
+
+    public Staff(String name, String address, String school, double pay) {
+        super(name, address);
+        this.school = school;
+        this.pay = pay;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public double getPay() {
+        return pay;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public void setPay(double pay) {
+        this.pay = pay;
+    }
+
+    @Override
+    public String toString() {
+        return "Staff{" +
+                "Person{" +
+                "name='" + getName() + '\'' +
+                ", address='" + getAddress() + '\'' +
+                '}' +
+                ", school='" + school + '\'' +
+                ", pay=" + pay +
+                '}';
+    }
+}
+
