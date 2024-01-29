ce05802391093e0439bf8cd76156640b0ea464e9
Wahyu Satrio Widodo <74386472+wahyusw813@users.noreply.github.com>
2024-01-29 07:06:46 +0700

    Create Ball.java

---
+++ Pertemuan3/Ball/Ball.java
@@ -0,0 +1,73 @@
+public class Ball {
+    private float x;
+    private float y;
+    private int radius;
+    private float xDelta;
+    private float yDelta;
+
+    public Ball(float x, float y, int radius, float xDelta, float yDelta) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.xDelta = xDelta;
+        this.yDelta = yDelta;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public float getxDelta() {
+        return xDelta;
+    }
+
+    public float getyDelta() {
+        return yDelta;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public void setxDelta(float xDelta) {
+        this.xDelta = xDelta;
+    }
+
+    public void setyDelta(float yDelta) {
+        this.yDelta = yDelta;
+    }
+    
+    public void move() {
+        x += xDelta;
+        y += yDelta;
+    }
+
+    public void reflectHorizontal() {
+        xDelta = -xDelta;
+    }
+
+    public void reflectVertical() {
+        yDelta = -yDelta;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Ball[(%.2f,%.2f), speed=(%.2f,%.2f)]", x, y, xDelta, yDelta);
+   }
+}
